2af5572694d842dce5f070420eeaa5ac9ec9aa22
webealin <weber-alina@hotmail.de>
2025-08-31 17:52:39 +0200

    added tflint

--- modules/debian-13-vm/providers.tf
+++ modules/debian-13-vm/providers.tf
@@ -3,7 +3,9 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "bpg/proxmox"
+      source  = "bpg/proxmox"
+      version = "0.82.1"
     }
   }
+  required_version = ">= v1.10.5"
 }
--- versions.tf
+++ versions.tf
@@ -6,5 +6,10 @@ terraform {
       source  = "bpg/proxmox"
       version = "0.82.1"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.3"
+    }
   }
+  required_version = ">= v1.10.5"
 }
