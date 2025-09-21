13658261d38af2f98f264547d4f858eddc6c37b3
webealin <weber-alina@hotmail.de>
2025-09-21 18:41:30 +0200

    feat: :sparkles: adds a remote state backend
    This adds moving to GitLab as remote state backend

---
+++ backend.tf
@@ -0,0 +1,12 @@
+# backend.tf
+
+terraform {
+  backend "http" {
+    address        = "https://gitlab.internal.webernet-online.de/api/v4/projects/2/terraform/state/production"
+    lock_address   = "https://gitlab.internal.webernet-online.de/api/v4/projects/2/terraform/state/production/lock"
+    unlock_address = "https://gitlab.internal.webernet-online.de/api/v4/projects/2/terraform/state/production/lock"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+    retry_wait_min = 5
+  }
+}
