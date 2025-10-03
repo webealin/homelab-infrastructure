2f399d9dee511a52501d660042fb121404f7fe1d
webealin <weber-alina@hotmail.de>
2025-10-03 13:57:47 +0200

    fix: :green_heart: fixes wrong remote state backend address
    After reinstalling gitlab the project ID changed, so the remote state backend is project ID 1 now

--- backend.tf
+++ backend.tf
@@ -2,9 +2,9 @@
 
 terraform {
   backend "http" {
-    address        = "https://gitlab.internal.webernet-online.de/api/v4/projects/2/terraform/state/production"
-    lock_address   = "https://gitlab.internal.webernet-online.de/api/v4/projects/2/terraform/state/production/lock"
-    unlock_address = "https://gitlab.internal.webernet-online.de/api/v4/projects/2/terraform/state/production/lock"
+    address        = "https://gitlab.internal.webernet-online.de/api/v4/projects/1/terraform/state/production"
+    lock_address   = "https://gitlab.internal.webernet-online.de/api/v4/projects/1/terraform/state/production/lock"
+    unlock_address = "https://gitlab.internal.webernet-online.de/api/v4/projects/1/terraform/state/production/lock"
     lock_method    = "POST"
     unlock_method  = "DELETE"
     retry_wait_min = 5
